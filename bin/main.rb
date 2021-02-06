279d24be5025c41fd5e6935af2420da1c3ba4fcd
ybadmus <yusif.badmus@gmail.com>
2021-02-06 19:56:10 +0000

    Add users interface for players

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,52 @@
 #!/usr/bin/env ruby
 
-puts 'Hello world!'
+class Users
+
+  def initialize (player1, player2)
+    @player_1 = player1
+    @player_2 = player2
+    @counts = 0
+    @next_player = false
+    first_player_selections = []
+    second_player_selections = []
+  end
+
+  public
+  def turns 
+    if !@next_player
+      puts "Your turn - Player 1"
+      first_player_selections = gets.chomp()
+      @next_player = true
+    else 
+      puts "Your turn - Player 2"
+      second_player_selections = gets.chomp()
+      @next_player = false
+  end
+
+  private
+  def winner(player_name)
+    puts "#{player_name} has won!"
+  end
+
+  private
+  def draw
+    puts "It's a draw, no player won after the game"
+  end
+
+  public
+  def gameover
+    puts "Gameover!"
+  end
+end
+
+
+puts "Press any key to start game"
+gets.chomp()
+puts "The game has started"
+puts "Player 1: Choose and nickname"
+player_1 = gets.chomp()
+puts "Player 2: Choose and nickname"
+player_2 = gets.chomp()
+
+users = Users.new(player_1, player_2)
+users.turns
