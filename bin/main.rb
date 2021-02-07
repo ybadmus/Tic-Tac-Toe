49ace3e3f302c09eee80f3482a7a7970a89d07c1
ybadmus <yusif.badmus@gmail.com>
2021-02-07 00:10:23 +0000

    issues raised by CR resolved, stylint errors as well

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,14 @@
 #!/usr/bin/env ruby
 
 class Users
+  MAX_COUNTS = 9
+  SUCCESS_OUTCOMES = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+    [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+  ].freeze
+
   def initialize(player1, player2)
+    @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     @player1 = player1
     @player2 = player2
     @counts = 0
@@ -10,28 +17,51 @@ class Users
     @second_player_selections = []
   end
 
-  def turns
-    if !@next_player
-      puts 'Your turn - Player 1'
-      @first_player_selections << gets.chomp
-      @next_player = true
-    else
-      puts 'Your turn - Player 2'
-      @second_player_selections << gets.chomp
-      @next_player = false
+  def take_turns
+    (0..MAX_COUNTS).each do |_i|
+      puts "Player #{@next_player ? '2' : '1'}, Choose your move!"
+      available_moves
+      user_input = gets.chomp
+      unless @cells.include? user_input.to_i
+        puts 'Invalid move!'
+        next
+      end
+      @cells.reject! { |item| item == user_input.to_i }
+      @next_player ? @second_player_selections << user_input.to_i : @first_player_selections << user_input.to_i
+      @counts += 1
+      break if check_outcome
     end
   end
 
   private
 
-  def winner(player_name)
-    puts "#{player_name} has won!"
-    gameover
+  def check_outcome
+    active_selections = @next_player ? @second_player_selections : @first_player_selections
+    if SUCCESS_OUTCOMES.include? active_selections
+      player_name = @next_player ? @player2 : @player1
+      puts "#{player_name} has won!"
+      gameover
+      true
+    elsif (SUCCESS_OUTCOMES.include? active_selections) && @counts == MAX_COUNTS
+      puts "It's a tie!"
+      gameover
+      true
+    else
+      @next_player = !@next_player
+      false
+    end
   end
 
-  def draw
-    puts "It's a tie!"
-    gameover
+  def available_moves
+    puts <<-HEREDOC
+      ---+---+---
+      #{@cells[0]} | #{@cells[1]} | #{@cells[2]}
+      ---+---+---
+      #{@cells[3]} | #{@cells[4]} | #{@cells[5]}
+      ---+---+---
+      #{@cells[6]} | #{@cells[7]} | #{@cells[8]}
+      ---+---+---
+    HEREDOC
   end
 
   public
@@ -41,13 +71,12 @@ class Users
   end
 end
 
-puts 'Press any key to start game'
+puts 'Press enter to start game'
 gets.chomp
-puts 'The game has started'
 puts 'Player 1: Choose and nickname'
 player1 = gets.chomp
 puts 'Player 2: Choose and nickname'
 player2 = gets.chomp
 
 users = Users.new(player1, player2)
-users.turns
+users.take_turns
