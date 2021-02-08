ad22d20a744bbde3ff7db52334e4bc57670a08ae
ybadmus <yusif.badmus@gmail.com>
2021-02-08 00:27:15 +0000

    disable cops for cyclomatic

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,6 +1,6 @@
-class Logic
-  attr_accessor :MAX_COUNTS
+# rubocop:disable Metrics/CyclomaticComplexity
 
+class Logic
   SUCCESS_OUTCOMES = [
     [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
     [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
